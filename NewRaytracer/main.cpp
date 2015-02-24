62ef10561d84c9ff9ad0cd10bea5990a6cdf9782
Ellis Sparky Hoag <ellishoag@Elliss-MacBook-Pro.local>
2015-02-23 22:57:23 -0600

    Initial Commit

---
+++ NewRaytracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NewRaytracer
+//
+//  Created by Ellis Sparky Hoag on 2/23/15.
+//  Copyright (c) 2015 Ellis Sparky Hoag. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
